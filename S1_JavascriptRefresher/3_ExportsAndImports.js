02f8bbcb8aef1a7aa041340f6c95e553f2de0b8a
Geetham15 <Geetham15@gmail.com>
2022-12-15 14:10:24 -0700

    Exports and Imports

---
+++ S1_JavascriptRefresher/3_ExportsAndImports.js
@@ -0,0 +1,27 @@
+//Exports & Imports (Modules)
+/*
+person.js
+
+const person = {
+    name:'Max'
+}
+export default person
+
+utility.js
+export const clean = () => {...}
+export const baseData = 10;
+
+app.js
+// default export
+import person from './person.js'
+import prs from './person.js'
+
+import { baseData } from './utility.js'
+import { clean } from './utility.js'
+
+//named export
+import { smth } from './utility.js'
+import { smth as Smth } from './utility.js'
+import * as bundled from './utility.js'
+
+*/
