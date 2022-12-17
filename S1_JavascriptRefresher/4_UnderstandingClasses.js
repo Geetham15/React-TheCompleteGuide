d5c6f7f2afa72900a4eb2b0e471e1b027c642eb3
Geetham15 <Geetham15@gmail.com>
2022-12-17 11:05:43 -0700

    Understanding Classes

---
+++ S1_JavascriptRefresher/4_UnderstandingClasses.js
@@ -0,0 +1,14 @@
+//Classes
+
+class Person {
+    name = 'Max' //property
+    //call = () => {...} //method
+}
+// Usage (constructor functions anyone?)
+const myPerson = new Person()
+myPerson.call()
+console.log(myPerson.name)
+// Inheritance (prototypes anyone?)
+class Person extends Master {
+    
+}
