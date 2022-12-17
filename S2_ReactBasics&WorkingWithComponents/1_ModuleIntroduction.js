d08a5c219018f92bf9a18a5e8e178cf06bc648cb
Geetham15 <Geetham15@gmail.com>
2022-12-17 14:15:49 -0700

    Module Introduction

---
+++ S2_ReactBasics&WorkingWithComponents/1_ModuleIntroduction.js
@@ -0,0 +1,31 @@
+//Module Content
+/*
+React core syntax & JSX
+Working with Components
+Working with Data
+*/
+/*
+React is a javascript library for building user interfaces
+React makes building complex, interactive and reactive user interfaces simpler
+React is all about "Components"
+Because all user interfaces in the end are made up of components
+
+Why Components?
+Reusability
+Separation of concerns
+Don't repeat yourself
+Don't do too many things in one and the same place (function)
+Split a big chunk of code into multiple smaller functions
+
+
+What is a "Components"?
+
+How is A Component Built?
+HTML + CSS + Javascript => React
+
+React & Components
+React allow you to create re-usable and reactive components consisting of HTML and javascript ( and CSS)
+|-> Declarative Approach -> Define the desired target state(s) and let React figure out the actual JavaScript DOM Instructions
+Build your own, custom HTML Elements
+
+*/
