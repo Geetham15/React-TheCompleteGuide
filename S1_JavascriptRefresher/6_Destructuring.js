42e5731d10d9d3c841e37f2ec6885220f8845fc7
Geetham15 <Geetham15@gmail.com>
2022-12-17 11:25:30 -0700

    Destructuring

---
+++ S1_JavascriptRefresher/6_Destructuring.js
@@ -0,0 +1,11 @@
+// Destructuring - Easily extract array elements or object properties and store them in variables
+
+//Array Destructing
+[a, b] = ['Hello', 'Max']
+console.log(a) //Hello
+console.log(b) //Max
+
+//Object Destructuring
+//{name} = {name:'Max', age:28 }
+//console.log(name) //Max
+//console.log(age) //undefined
