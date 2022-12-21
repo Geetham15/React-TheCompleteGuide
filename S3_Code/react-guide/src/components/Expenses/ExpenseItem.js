235b570b688cab22024860cec120f65407373da7
Geetham15 <Geetham15@gmail.com>
2022-12-21 14:11:59 -0700

    Listening to Events & Working with Event Handlers

--- S3_Code/react-guide/src/components/Expenses/ExpenseItem.js
+++ S3_Code/react-guide/src/components/Expenses/ExpenseItem.js
@@ -3,7 +3,9 @@ import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card';
 const ExpenseItem =(props) =>{
-    
+    const clickHandler = () => {
+      console.log('Clicked!!!!!')
+    }
   return (
     
     <Card className='expense-item'>
@@ -13,6 +15,7 @@ const ExpenseItem =(props) =>{
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
