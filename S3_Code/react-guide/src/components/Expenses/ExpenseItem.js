5cf04588afbd7e78d7f41c0d657dfbbc6c80e872
Geetham15 <Geetham15@gmail.com>
2022-12-21 14:33:05 -0700

    Working with State

--- S3_Code/react-guide/src/components/Expenses/ExpenseItem.js
+++ S3_Code/react-guide/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card';
+
 const ExpenseItem =(props) =>{
+  const [title, setTitle ] = useState(props.title)
+  
+
     const clickHandler = () => {
-      console.log('Clicked!!!!!')
+      setTitle('Updated!')
+      console.log(title) //old value is logged
     }
+
   return (
     
     <Card className='expense-item'>
       {/* <div>{props.date.toISOString()}</div> */}
       <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
