206045f55f0ef144ec4a82560b394c5a6c73115f
Geetham15 <Geetham15@gmail.com>
2022-12-22 13:53:14 -0700

    Adding Two-way Binding

--- S3_Code/react-guide/src/components/NewExpense/ExpenseForm.js
+++ S3_Code/react-guide/src/components/NewExpense/ExpenseForm.js
@@ -44,27 +44,41 @@ const ExpenseForm = () => {
   }
   const submitHandler = (event) => {
     event.preventDefault();
+
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
       date: new Date(enteredDate)
     }
+
     console.log(expenseData)
+    
+    setEnteredTitle('')
+    setEnteredAmount('')
+    setEnteredDate('')
   }
   return (
     <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
             <label>Title</label>
-            <input type='text' onChange={titleChangeHandler}/>
+            <input type='text' 
+            value= {enteredTitle}
+            onChange={titleChangeHandler}/>
         </div>
         <div className='new-expense__control'>
             <label>Amount</label>
-            <input type='number' onChange={amountChangeHandler} min="0.01"  step="0.01" />
+            <input type='number' value= {enteredAmount}
+            onChange={amountChangeHandler} 
+            min="0.01"  
+            step="0.01" />
         </div>
         <div className='new-expense__control'>
             <label>Date</label>
-            <input type='date' onChange= {dateChangeHandler} min="2019-01-01" max="2022-12-31" />
+            <input type='date' value={enteredDate}
+            onChange= {dateChangeHandler} 
+            min="2019-01-01" 
+            max="2022-12-31" />
         </div>
       </div>
       <div className='new-expense__actions'>
