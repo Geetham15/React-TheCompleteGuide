b6c47fe018c5c2fe9a5fdba8a30aa676ca5be7f1
Geetham15 <Geetham15@gmail.com>
2022-12-24 18:22:04 -0700

    Rendered list display according to the filter year

--- S3_Code/react-guide/src/components/Expenses/Expenses.js
+++ S3_Code/react-guide/src/components/Expenses/Expenses.js
@@ -12,12 +12,16 @@ const Expenses=(props)=> {
     console.log(selectedYear)
     setFilteredYear(selectedYear)
   }
-
+  
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
   return (
     <div> 
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      { props.items.map( (expense) => (
+    
+      { filteredExpenses.map((expense) => (
         <ExpenseItem 
           key={expense.id}
           title={expense.title}           
