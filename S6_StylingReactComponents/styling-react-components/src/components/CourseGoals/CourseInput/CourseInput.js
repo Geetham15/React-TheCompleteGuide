2833419900cfe3805a00306e48b340ceb54e123d
Geetham15 <Geetham15@gmail.com>
2022-12-27 13:17:01 -0700

    Styled components and dynamic props

--- S6_StylingReactComponents/styling-react-components/src/components/CourseGoals/CourseInput/CourseInput.js
+++ S6_StylingReactComponents/styling-react-components/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,45 @@
 import React, { useState } from 'react';
-
+import styled from 'styled-components'
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+
+const FormControl = styled.div`
+
+  margin: 0.5rem 0;
+
+
+& label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+  color: ${props => props.invalid? 'red':'black'};
+}
+
+& input {
+  display: block;
+  width: 100%;
+  // border: 1px solid #ccc;
+  border: 1px solid ${props => (props.invalid? 'red' : '#ccc')};
+  background: ${props => (props.invalid? '#ffd7d7':'transparent')};
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+}
+
+& input:focus {
+  outline: none;
+  background: #fad0ec;
+  border-color: #8b005d;
+}
+// &.invalid input {
+//   border-color: red;
+//   background: salmon;
+// }
+// &.invalid label {
+//   color: red;
+// }
+
+`;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -39,17 +77,18 @@ const CourseInput = props => {
       </div> 
       */}
 
-      <div className={`form-control ${!isValid ? 'invalid' :''}`}>
+      {/* <div className={`form-control ${!isValid ? 'invalid' :''}`}> */}
       {/* Setting CSS Classes Dynamically */}
-      <label>Course Goal</label>
-      <input 
-            type="text" 
-            onChange={goalInputChangeHandler} 
-      />
-      </div>
-
+            {/* <FormControl className={!isValid && 'invalid'}> */}
+            <FormControl invalid={!isValid} >
+              <label>Course Goal</label>
+              <input 
+                    type="text" 
+                    onChange={goalInputChangeHandler} 
+              />
+            </FormControl>
             <Button type="submit">Add Goal</Button>
-          </form>
+      </form>
         );
 };
 
