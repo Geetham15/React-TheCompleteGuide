4e77439efb8fc8d20ce4a05bdfcdb7948a656ff4
Geetham15 <Geetham15@gmail.com>
2022-12-29 12:25:36 -0700

    Creating Wrapper component

--- S8_UserPracticeProject/user-guide/src/components/Users/AddUser.js
+++ S8_UserPracticeProject/user-guide/src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper'
 import classes from './AddUser.module.css'
 
 const AddUser = (props) => {
@@ -43,7 +44,7 @@ const AddUser = (props) => {
     }
 
   return (
-    <div>
+    <Wrapper>
     { error && (
         <ErrorModal 
             title={error.title} 
@@ -66,7 +67,7 @@ const AddUser = (props) => {
         <Button type='submit'>Add User</Button>
         </form>
     </Card>
-    </div>
+    </Wrapper>
   )
 }
 
