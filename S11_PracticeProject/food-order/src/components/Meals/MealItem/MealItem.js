5891d12e0097630c12ab8e9589eb386e560afb58
Geetham15 <Geetham15@gmail.com>
2023-01-14 10:04:29 -0700

    Working with Refs & Forward Refs

--- S11_PracticeProject/food-order/src/components/Meals/MealItem/MealItem.js
+++ S11_PracticeProject/food-order/src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import MealItemForm from "./MealItemForm";
-import classes from './MealItem.module.css'
+import classes from './MealItem.module.css';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
+    const cartCtx = useContext(CartContext);
     const price = `$${props.price.toFixed(2)}`
+    const addItemToCartHandler = amount => {
+      cartCtx.addItem({
+        id: props.id,
+        name: props.name,
+        amount: amount,
+        price: props.price
+      });
+    }
   return (
     <li className={classes.meal}>
       <div>
@@ -12,7 +22,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id}/>
+        <MealItemForm id={props.id} onAddToCart={addItemToCartHandler}/>
       </div>
     </li>
   );
